0ed1e115767e84efc52ab8f0a8e3d308fcd2f470
AsarTheGreat <59713469+AsarTheGreat@users.noreply.github.com>
2024-07-03 01:08:20 -0400

    Update TodoList.jsx

--- todo/src/components/TodoList.jsx
+++ todo/src/components/TodoList.jsx
@@ -6,7 +6,7 @@ export default function TodoList({todos, setTodos})
 {
     //Another method of sorting
     //Needed to use it in the map method for showing the list
-    const sortedTodos = todos.slice().sort((a,b) => Number(a.done)- Number(b.done));
+    const sortedTodos = todos.filter((t) => !t.done).concat(todos.filter((t) => t.done));
     console.log(sortedTodos);
 
     //Need to pass setTodos in order to perform the deletion method
