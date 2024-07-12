ea6d7ae3e4ae9627c1293188711f13cea62ed744
thepolokie-Mac <thepolokie-mac@thepoloeMacsAir.fios-router.home>
2024-07-12 01:23:35 -0400

    Corrected Fruit and Fruits components

--- basics/src/Components/Fruit.jsx
+++ basics/src/Components/Fruit.jsx
@@ -3,7 +3,7 @@ export default function Fruit({id, name, count})
     //{(count > 10) ? <li key={id} id={id}>{name} {count}</li> : ""}
     return(
         <div>
-            <li key={f.id} id={f.id}>{f.name} {f.count}</li>            
+            <li key={id} id={id}>{name} {count}</li>            
         </div>
     );
 }
